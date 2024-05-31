317e996581d02c9e9fe8496d08d5d5973f44ba01
Saravanan M <sarvan.m20@gmail.com>
2024-05-31 22:01:52 +0530

    SharkContainer component updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
+import SharkContainer from './components/SharkContainer';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <SharkContainer />
+      {/* <Navbar />
+      <HeroSection />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/SharkContainer.jsx
+++ src/components/SharkContainer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/SharkContainer.scss";
+import closeIcon from "../images/closeIcon.svg";
 
 const SharkContainer = () => {
-  return (
-    <div>SharkContainer</div>
-  )
-}
+  const [showShark, setShowShark] = useState(true);
 
-export default SharkContainer
+  const hanldeClick = () => {
+    setShowShark(false);
+  };
+
+  return (
+    <div className={showShark ? "shark-container" : "disable-shark-container"}>
+      <img
+        src="https://www.plusgold.in/images/banner_shark_tank_india.png"
+        alt="sharkTankBanner"
+        className="shark-img"
+      />
+      <div className="close-btn" onClick={hanldeClick}>
+        <img src={closeIcon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default SharkContainer;

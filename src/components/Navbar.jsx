ec5ee2edcfea4eeeb98f519ba70f4f69491c3471
Saravanan M <sarvan.m20@gmail.com>
2024-06-02 09:59:31 +0530

    navbar responsiveness done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar.scss";
 import logo from "../images/logo.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [mobMenuOpen, setMobMenuOpen] = useState(false);
   return (
     <div className="navbar-container">
       <nav>
@@ -10,12 +13,27 @@ const Navbar = () => {
           <img src={logo} alt="" />
         </div>
         <div className="nav-items">
-          <div>Start Saving</div>
-          <div>Gold Rush Festival</div>
-          <div>Partner with us</div>
+          <div className="each-nav-item">Start Saving</div>
+          <div className="each-nav-item">Gold Rush Festival</div>
+          <div className="each-nav-item">Partner with us</div>
           <button className="download-btn">Download App</button>
+          <div
+            className="ham-menu"
+            onClick={() => setMobMenuOpen(!mobMenuOpen)}
+          >
+            {!mobMenuOpen ? <GiHamburgerMenu /> : <IoClose />}
+          </div>
         </div>
       </nav>
+      {mobMenuOpen ? (
+        <div className="mob-nav-items">
+          <div className="each-mob-nav-item">Start Saving</div>
+          <div className="each-mob-nav-item">Gold Rush Festival</div>
+          <div className="each-mob-nav-item">Partner with us</div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };

b863ad78569f23c02f8a95d5c502a86cee60a315
Saravanan M <sarvan.m20@gmail.com>
2024-06-03 18:31:33 +0530

    video added

--- src/components/SharkTankContainer.jsx
+++ src/components/SharkTankContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/SharkTankContainer.scss";
-import thumbnail from '../images/sharktank.svg';
+import thumbnail from "../images/sharktank.svg";
 import { FaArrowRight } from "react-icons/fa6";
 
 const SharkTankContainer = () => {
@@ -24,13 +24,24 @@ const SharkTankContainer = () => {
             </div>
             <div className="mt-4">
               <p>
-                Watch our Shark <br /> <span> Tank pitch&nbsp; <FaArrowRight /></span>
+                Watch our Shark <br />{" "}
+                <span>
+                  {" "}
+                  Tank pitch&nbsp; <FaArrowRight />
+                </span>
               </p>
             </div>
           </div>
           <div className="sharktank-vid-div">
             <div>
-                <img src={thumbnail} alt="" />
+              {/* <img src={thumbnail} alt="" /> */}
+              <video controls>
+                <source
+                  src="https://getplus-backend-prod.s3.ap-south-1.amazonaws.com/media/Plus+Gold+_+Thankyou+for+watching+.mp4"
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
             </div>
           </div>
         </div>
